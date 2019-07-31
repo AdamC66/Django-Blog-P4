6753b8fd526f539465bb24e6dc8ec5f9b4fc4427
adamc66 <adam.cote66@gmail.com>
2019-07-31 09:59:39 -0400

    made migrations for comment model

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,9 @@ class Article(models.Model):
     author = models.CharField(max_length = 63, null = False)
     def __str__(self):
         return (f'{self.title}, By: {self.author}')
+
+class Comment(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
+    picture = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
